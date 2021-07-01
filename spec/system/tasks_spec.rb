a559cee4a91cd7a4072e3a4f5b7aa3422b91d6b5
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-01 20:55:53 +0900

    [fix] rspec shared_examples_for

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -7,7 +7,7 @@ describe 'タスクの管理機能', type: :system do
   
   before do
     #作成者がユーザーAであるタスクを作成しておく
-    FactoryBot.create(:task, name: '最初のテストタスク', user: user_a)
+    FactoryBot.create(:task, name: '最初のタスク', user: user_a)
 
     #ユーザーAでログインする
     visit login_path
@@ -16,21 +16,22 @@ describe 'タスクの管理機能', type: :system do
     click_button 'ログインする'
   end
 
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク'}
+  end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        #作成済みのタスクの名称が画面上に表示されることを確認
-        expect(page).to have_content '最初のテストタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
     context 'ユーザーBがログインしているとき' do
       let(:login_user) { user_b }
       
       it 'ユーザーAが作成したタスクが表示されない' do
         #ユーザーAが作成したタスクの名前が画面上に表示されていないことを確認
-        expect(page).to have_no_content '最初のテストタスク'
+        expect(page).to have_no_content '最初のタスク'
       end
     end
   end
@@ -43,9 +44,7 @@ describe 'タスクの管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 end
